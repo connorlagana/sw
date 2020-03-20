314550498172eebee29bf2532f09323d10d7cc70
Connor Lagana <connor.lagana@gmail.com>
2020-03-20 12:34:22 -0400

    nav update

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,23 @@
 import React from "react";
+import profile from "../images/profile.png";
+import cart from "../images/cart.png";
 
 function Nav() {
   return (
     <div className="Nav">
-      <div>Free shipping Over $90. U.S. Only</div>
-      <div>
-        <div>Suburban Wave</div>
-        <div></div>
+      <div className="topNav">Free shipping Over $90. U.S. Only</div>
+      <div className="bottomNav">
+        <div className="leftNav">Suburban Wave</div>
+        <div className="centerNav">
+          <div>New</div>
+          <div>Women</div>
+          <div>Men</div>
+          <div>Misc.</div>
+        </div>
+        <div className="rightNav">
+          <img src={profile} />
+          <img src={cart} />
+        </div>
       </div>
     </div>
   );
