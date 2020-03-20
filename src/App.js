be28357c3c8f14ce64fb4233fa6ac302bfcad49e
Connor Lagana <connor.lagana@gmail.com>
2020-03-20 12:19:07 -0400

    nav bar initiated

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
-import "./App.css";
+import "./App.scss";
+
+import Nav from "./components/Nav.js";
 
 class App extends Component {
   constructor() {
     super();
   }
   render() {
-    return <div className="App">
-      hello world!
-    </div>;
+    return (
+      <div className="App">
+        <Nav />
+      </div>
+    );
   }
 }
 
---
+++ src/components/Nav.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <div className="Nav">
+      <div>Free shipping Over $90. U.S. Only</div>
+      <div>
+        <div>Suburban Wave</div>
+        <div></div>
+      </div>
+    </div>
+  );
+}
+
+export default Nav;
