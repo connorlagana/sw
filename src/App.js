82ce35a8ee65ff61c7e568bc270c324fda394b33
Connor Lagana <connor.lagana@gmail.com>
2020-03-20 13:49:51 -0400

    gg

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from "./logo.svg";
 import "./App.scss";
 
 import Nav from "./components/Nav.js";
+import Hero from "./components/Hero.js";
 
 class App extends Component {
   constructor() {
