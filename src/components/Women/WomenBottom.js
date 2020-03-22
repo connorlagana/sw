bfc6d19bc09cce001e959d1013dd9c9130ab28e0
Connor Lagana <connor.lagana@gmail.com>
2020-03-22 17:08:20 -0400

    side bar womens

--- src/components/Women/WomenBottom.js
+++ src/components/Women/WomenBottom.js
@@ -14,6 +14,32 @@ function WomenBottom() {
           <button id="category">Sale</button>
           <button id="viewAll">View All</button>
         </div>
+        <div className="colors">
+          <div id="colorTitle">Color</div>
+          <div className="colorButtons">
+            <button className="black sqBut" />
+            <button className="red sqBut" />
+            <button className="gray sqBut" />
+            <button className="blue sqBut" />
+            <button className="brown sqBut" />
+            <button className="white sqBut" />
+            <button className="green sqBut" />
+          </div>
+        </div>
+        <div className="colors">
+          <div id="colorTitle">Size</div>
+          <div className="sizeButtons">
+            <button>XS</button>
+            <button>S</button>
+            <button>M</button>
+            <button>L</button>
+            <button>XL</button>
+            <button>0X</button>
+            <button>1X</button>
+            <button>2X</button>
+            <button>3X</button>
+          </div>
+        </div>
       </div>
       <div className="rightWomenBottomDiv">Right Div</div>
     </div>
