8512b4c51c915091ec6c4fafa0336cc6aca73c56
Connor Lagana <connor.lagana@gmail.com>
2020-03-22 10:53:53 -0400

    nav bar resp

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,18 @@
 import React from "react";
+import firstModel from "../images/firstModel.jpg";
+import secondModel from "../images/secondModel.JPG";
 
 function Hero() {
   return (
     <div className="Hero">
-      
+      <div>
+        <img src={firstModel} />
+        <button>Shop Women</button>
+      </div>
+      <div>
+        <img src={secondModel} />
+        <button>Shop Men</button>
+      </div>
     </div>
   );
 }
