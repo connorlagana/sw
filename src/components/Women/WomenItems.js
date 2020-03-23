d62853e5a4cdd5f12e01e39d96964fde0e4c7876
Connor Lagana <connor.lagana@gmail.com>
2020-03-22 20:32:54 -0400

    change pic

--- src/components/Women/WomenItems.js
+++ src/components/Women/WomenItems.js
@@ -11,9 +11,15 @@ function WomenItems(props) {
         {props.items &&
           props.items.map(item => (
             <div className="itemInd">
-              <img src={item.firstPic} />
-              <div id="itemName">{item.name}</div>
-              <div id="itemPrice">${item.price}.00</div>
+              <img
+                src={item.firstPic}
+                onMouseOver={e => (e.currentTarget.src = item.secondPic)}
+                onMouseOut={e => (e.currentTarget.src = item.firstPic)}
+              />
+              <div className="bottomInd">
+                <div id="itemName">{item.name}</div>
+                <div id="itemPrice">${item.price}.00</div>
+              </div>
             </div>
           ))}
       </div>
