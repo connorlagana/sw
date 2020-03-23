93a4e8a88277284af28dc461824b39a811450eea
Connor Lagana <connor.lagana@gmail.com>
2020-03-22 21:59:00 -0400

    contact form initiated

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,34 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
 
-function ContactUs() {
-  return (
-    <div className="ContactUs">
-      <div id="contactTitle">Contact Us</div>
-      <div>
-        <div>
+class ContactUs extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div className="ContactUs">
+        <div id="contactTitle">Contact Us</div>
+        <div className="contactBottom">
           <div>
-            <div>Customer Service</div>
-            <div>Email: customerservice@suburbanwave.com</div>
-            <div>Phone: 973-783-3799</div>
+            <div>
+              <div>Customer Service</div>
+              <div>Email: customerservice@suburbanwave.com</div>
+              <div>Phone: 973-783-3799</div>
+            </div>
+            <div>
+              <div>Address</div>
+              <div>Suburban Wave</div>
+              <div>87 Tower Mountain Drive</div>
+              <div>Bernardsville, NJ 07924</div>
+            </div>
+            <div>
+              <div>Wholesale Inquiries</div>
+              <div>Email: info@suburbanwave.com</div>
+              <div>Phone: 973-783-3799</div>
+            </div>
           </div>
-          <div>
-            <div>Address</div>
-            <div>Suburban Wave</div>
-            <div>87 Tower Mountain Drive</div>
-            <div>Bernardsville, NJ 07924</div>
-          </div>
-          <div>
-            <div>Wholesale Inquiries</div>
-            <div>Email: info@suburbanwave.com</div>
-            <div>Phone: 973-783-3799</div>
-          </div>
-        </div>
-        <div>
-          <form></form>
+          <form className="contactForm">
+            <input type="text" name="name" placeholder="Name" />
+            <input type="email" name="email" placeholder="Email" />
+            <input type="text" name="message" placeholder="Message" />
+            <input type="submit" value="Submit" />
+          </form>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default ContactUs;
