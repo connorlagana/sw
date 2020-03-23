bcae6feaa4cc08fe959f714297fa4d1ccddf5730
Connor Lagana <connor.lagana@gmail.com>
2020-03-23 14:08:33 -0400

    let me down

--- src/components/Extra/OurStory.js
+++ src/components/Extra/OurStory.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import secondPic from "../../images/OurStory/girlsAbout.jpg";
 import firstPic from "../../images/OurStory/goodVibesAbout.jpg";
+import surfer from "../../images/OurStory/surfer.jpg";
 
 function OurStory() {
   return (
@@ -13,6 +14,23 @@ function OurStory() {
         <div className="fosr">
           <img src={secondPic} />
           <label>Life</label>
+        </div>
+      </div>
+
+      <div className="secondOS">
+        <img src={surfer} />
+        <div className="sosr">
+          <label>Blame it on my youth</label>
+          <div className="tnl">
+            <div>The</div>
+            <div>New Luxury</div>
+          </div>
+
+          <div>
+            From the beach - to the gym - to the office - we make clothes that
+            reflect today's on the go looks... along with a splash of positivity
+            and humor.
+          </div>
         </div>
       </div>
     </div>
