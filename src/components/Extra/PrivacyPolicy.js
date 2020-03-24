b8ac511dfdc69d782e2f9bfdec033c150980a75e
Connor Lagana <connor.lagana@gmail.com>
2020-03-24 17:33:29 -0400

    full psycho

--- src/components/Extra/PrivacyPolicy.js
+++ src/components/Extra/PrivacyPolicy.js
@@ -6,33 +6,33 @@ function PrivacyPolicy() {
     <div className="PrivacyPolicy">
       <div id="privacyPolicyTitle">Privacy Policy</div>
       <div className="bottomPrivacy">
-        <div>
-          <div>Our Commitment To Your Privacy</div>
-          <div>
+        <div className="ppDiv">
+          <div id="ppTitle">Our Commitment To Your Privacy</div>
+          <div id="ppMessage">
             Your privacy is important to us. To better protect your privacy the
             following notice explains the information we collect, how it is
             used, how it is safeguarded, and how to contact us if you have any
             concerns.
           </div>
         </div>
-        <div>
-          <div>What Information Is Collected:</div>
-          <div>
+        <div className="ppDiv">
+          <div id="ppTitle">What Information Is Collected:</div>
+          <div id="ppMessage">
             As part of the order process, the following information is collected
             from shoppers: Name Shipping/Billing Address, Email address, Phone
             number, Credit/Debit Card Information.
           </div>
         </div>
-        <div>
-          <div>How That Information Is Used:</div>
-          <div>
+        <div className="ppDiv">
+          <div id="ppTitle">How That Information Is Used:</div>
+          <div id="ppMessage">
             We only use information to fill orders, contact buyers if they have
             questions, or to send special offers.
           </div>
         </div>
-        <div>
-          <div>Our Commitment To The Security of Your Data:</div>
-          <div>
+        <div className="ppDiv">
+          <div id="ppTitle">Our Commitment To The Security of Your Data:</div>
+          <div id="ppMessage">
             Your personal information is secure and will not be used in any way
             other than to fill orders, contact buyers if they have questions, or
             to send special offers.
