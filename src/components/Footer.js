b322316e931a92f83b27a2eb92bb4910362f2cbf
Connor Lagana <connor.lagana@gmail.com>
2020-04-07 01:52:50 -0400

    added scroll feature

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,52 +1,70 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import {
+  DirectLink,
+  Element,
+  Events,
+  animateScroll as scroll,
+  scrollSpy,
+  scroller
+} from "react-scroll";
 
-function Footer() {
-  return (
-    <div className="Footer">
-      <div className="leftFooter">
-        <div className="lfTop">
-          <div className="lftDiv">
-            <div id="lftTitle">Company Info</div>
-            <div className="lftUnder">
-              {/* <div>Stores</div> */}
-              <Link to="/contactus" id="lftLink">
-                Contact Us
-              </Link>
+class Footer extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  scrollToTop() {
+    scroll.scrollToTop();
+  }
+
+  render() {
+    return (
+      <div className="Footer">
+        <div className="leftFooter">
+          <div className="lfTop">
+            <div className="lftDiv">
+              <div id="lftTitle">Company Info</div>
+              <div className="lftUnder">
+                {/* <div>Stores</div> */}
+                <Link to="/contactus" id="lftLink">
+                  Contact Us
+                </Link>
+              </div>
+            </div>
+            <div className="lftDiv">
+              <div id="lftTitle">Customer Service</div>
+              <div className="lftUnder">
+                <Link to="/shipping" id="lftLink" onClick={this.scrollToTop}>
+                  Shipping
+                </Link>
+                <Link to="/contactus" id="lftLink">
+                  Terms & Conditions
+                </Link>
+                <Link to="/privacy-policy" id="lftLink">
+                  Privacy Policy
+                </Link>
+              </div>
+            </div>
+            <div className="lftDiv">
+              <div id="lftTitle">About Us</div>
+              <div className="lftUnder">
+                <Link to="/ourstory" id="lftLink" onClick={this.scrollToTop}>
+                  Our Story
+                </Link>
+              </div>
             </div>
           </div>
-          <div className="lftDiv">
-            <div id="lftTitle">Customer Service</div>
-            <div className="lftUnder">
-              <Link to="/shipping" id="lftLink">
-                Shipping
-              </Link>
-              <Link to="/contactus" id="lftLink">
-                Terms & Conditions
-              </Link>
-              <Link to="/privacy-policy" id="lftLink">
-                Privacy Policy
-              </Link>
-            </div>
-          </div>
-          <div className="lftDiv">
-            <div id="lftTitle">About Us</div>
-            <div className="lftUnder">
-              <Link to="/ourstory" id="lftLink">
-                Our Story
-              </Link>
-            </div>
+          <div className="lfBottom">
+            Copyright © 2020 Suburban Wave. All Rights Reserved.
           </div>
         </div>
-        <div className="lfBottom">
-          Copyright © 2020 Suburban Wave. All Rights Reserved.
+        <div className="rightFooter">
+          <div>thx 4 stopping by.</div>
         </div>
       </div>
-      <div className="rightFooter">
-        <div>thx 4 stopping by.</div>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Footer;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,41 +1,48 @@
-import React from "react";
+import React, { Component } from "react";
 import profile from "../images/profile.png";
 import cart from "../images/cart.png";
 import { Link } from "react-router-dom";
 
-function Nav() {
-  return (
-    <div className="Nav">
-      <div className="topNav">Free shipping Over $90. U.S. Only</div>
-      <div className="bottomNav">
-        <div className="leftNav">
-          <Link to="/" id="subrbanWave">
-            Suburban Wave
-          </Link>
-        </div>
-        <div className="centerNav">
-          <Link to="/new" id="navLink">
-            New
-          </Link>
-          <Link to="/women" id="navLink">
-            Women
-          </Link>
-          <Link to="/men" id="navLink">
-            Men
-          </Link>
-          <Link to="/youth" id="navLink">
-            Youth
-          </Link>
-        </div>
-        <div className="rightNav">
-          <Link to="login">
-            <img src={profile} />
-          </Link>
-          <img src={cart} />
+
+class Nav extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="Nav">
+        <div className="topNav">Free shipping Over $90. U.S. Only</div>
+        <div className="bottomNav">
+          <div className="leftNav">
+            <Link to="/" id="subrbanWave">
+              Suburban Wave
+            </Link>
+          </div>
+          <div className="centerNav">
+            <Link to="/new" id="navLink">
+              New
+            </Link>
+            <Link to="/women" id="navLink">
+              Women
+            </Link>
+            <Link to="/men" id="navLink">
+              Men
+            </Link>
+            <Link to="/youth" id="navLink">
+              Youth
+            </Link>
+          </div>
+          <div className="rightNav">
+            <Link to="login">
+              <img src={profile} />
+            </Link>
+            <img src={cart} />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Nav;
